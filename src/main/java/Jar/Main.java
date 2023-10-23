3e2d8a3f8a0e3b50550acccbeda6c0836ca66fb5
Kacper <kacpkuc@gmail.com>
2023-10-23 08:48:47 +0200

    File from bytes clean-up

--- src/main/java/Jar/Main.java
+++ src/main/java/Jar/Main.java
@@ -1,29 +1,17 @@
 package Jar;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String filePath = "C:\\Users\\Foxrabbit\\Desktop\\IT\\Java\\Programes\\testJar\\out\\artifacts\\testJar_jar\\testJar.jar";
-
-        byte[] bytes = Files.readAllBytes(Paths.get(filePath));
-
-
-//        or
+        String filePath = "Write/your/Path";
 
         File file = new File(filePath);
         byte[] bytes2 = Files.readAllBytes(file.toPath());
-
         System.out.println(Arrays.toString(bytes2));
-
-
-
-
     }
 }
